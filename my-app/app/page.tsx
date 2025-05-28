92bbb516c5d06413a625c4110588f3c81b3ec11e
Trey Nguyen <treynguyen.developer@gmail.com>
2025-05-28 07:43:27 -0500

    Created hero section

--- my-app/app/page.tsx
+++ my-app/app/page.tsx
@@ -1,17 +1,11 @@
-import Image from "next/image";
+import Hero from "@/components/Hero";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          src="/logo_initials.png"
-          alt="Trey Nguyen Design logo"
-          width={80}
-          height={20}
-          priority
-        />
-      </main>
-    </div>
+    <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
+      <div className="max-w-7xl w-full">
+        <Hero />
+      </div>
+    </main>
   );
 }
