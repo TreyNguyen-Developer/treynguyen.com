f2e304546a3530f359cbe77ac30d5e5811314774
Trey Nguyen <treynguyen.developer@gmail.com>
2025-05-28 21:47:34 -0500

    Push to prod

--- my-app/components/Hero.tsx
+++ my-app/components/Hero.tsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import { Spotlight } from './ui/Spotlight'
 import { TextGenerateEffect } from './ui/TextGenerateEffect'
+import MagicButton from './ui/MagicButton'
+import { FaLocationArrow } from 'react-icons/fa'
 
 const Hero = () => {
   return (
@@ -22,12 +24,16 @@ const Hero = () => {
 
           <TextGenerateEffect
             className='text-center text-[40px] md:text-5xl lg:text-6xl'
-            words='Transforming Concepts into Seamless Experiences'
+            words='Transforming Concepts into Seamless User Experiences'
           />
 
           <p className='text-center md:tracking-wider mb-4 text-sm md:text-lg lg:text-2xl'>
             Hi, I&apos;m Trey, a Product Designer based in Texas.
           </p>
+
+          <a href='/about'>
+            <MagicButton title='Show My Work' icon={<FaLocationArrow />} position='right'/>
+          </a>
         </div>
       </div>
     </div>
