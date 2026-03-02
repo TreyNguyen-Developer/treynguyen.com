91ede20c727bd9f3807af98a222c050d1b6d74f2
Trey Nguyen <treynguyen.developer@gmail.com>
2026-03-02 15:54:29 -0600

    Under contruction update

--- my-app/components/Hero.tsx
+++ my-app/components/Hero.tsx
@@ -28,12 +28,12 @@ const Hero = () => {
           />
 
           <p className='text-center md:tracking-wider mb-4 text-sm md:text-lg lg:text-2xl'>
-            Transforming Concepts into Seamless User Experiences
+            My website is currently under construction.
           </p>
 
-          <a href='#projects'>
+          {/* <a href='#projects'>
             <MagicButton title='Show My Work' icon={<FaLocationArrow />} position='right'/>
-          </a>
+          </a> */}
         </div>
       </div>
     </div>
