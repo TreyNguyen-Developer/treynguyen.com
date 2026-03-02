05551562ec5795853f9e6a75e547b2140a29645c
Trey Nguyen <treynguyen.developer@gmail.com>
2026-03-02 15:55:47 -0600

    Update page.tsx

--- my-app/app/page.tsx
+++ my-app/app/page.tsx
@@ -1,12 +1,12 @@
-import Approach from "@/components/Approach";
-import Clients from "@/components/Clients";
-import Experience from "@/components/Experience";
-import Footer from "@/components/Footer";
-import Grid from "@/components/Grid";
-import Hero from "@/components/Hero";
-import RecentProjects from "@/components/RecentProjects";
-import { FloatingNav } from "@/components/ui/FloatingNav";
-import { navItems } from "@/data";
+// import Approach from "@/components/Approach";
+// import Clients from "@/components/Clients";
+// import Experience from "@/components/Experience";
+// import Footer from "@/components/Footer";
+// import Grid from "@/components/Grid";
+// import Hero from "@/components/Hero";
+// import RecentProjects from "@/components/RecentProjects";
+// import { FloatingNav } from "@/components/ui/FloatingNav";
+// import { navItems } from "@/data";
 
 export default function Home() {
   return (
